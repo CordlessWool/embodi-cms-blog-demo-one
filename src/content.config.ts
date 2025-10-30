afdb3e9e84065ae2add3fc67820855afd2a4c9b1
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-10-30 17:51:40 +0100

    add collection with file loader

--- src/content.config.ts
+++ src/content.config.ts
@@ -20,6 +20,26 @@ const blogsCollection = defineCollection({
     }),
 });
 
+const dataFile = defineCollection({
+  loader: file("./src/content/data.yml"),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.date(),
+      author: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+      draft: z.boolean().optional(),
+      image: z
+        .object({
+          url: image(),
+          alt: z.string().optional(),
+        })
+        .optional(),
+    }),
+});
+
 export const collections = {
   blogs: blogsCollection,
+  data: dataFile,
 };
